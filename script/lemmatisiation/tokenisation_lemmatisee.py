aec3ef480258f49d44432678942d9e8235d7715c
Iceknith <dimi.ma@laposte.net>
2026-03-19 15:17:59 +0100

    tiny fix lemmatiser

---
+++ script/lemmatisiation/tokenisation_lemmatisee.py
@@ -0,0 +1,7 @@
+
+
+def cree_tokens_lemmatisee(token_file:str, lems_file:str, output_file:str):
+    pass
+
+if __name__ == "__main__":
+    cree_tokens_lemmatisee("output/lemmatisation/tokens.txt", "output/lemmatisation/lems_spacy.txt")
