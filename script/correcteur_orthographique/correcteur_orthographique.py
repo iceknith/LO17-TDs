b1f8a4342c8d73f9c6030e12dbee9198068ae35d
tyftyf-lab <tyfenn.forner@hotmail.com>
2026-03-26 17:12:13 +0100

    specific entity test and belongs to index test

--- script/correcteur_orthographique/correcteur_orthographique.py
+++ script/correcteur_orthographique/correcteur_orthographique.py
@@ -4,7 +4,23 @@ RECHERCHE_PREFIXE_SEUIL_MIN = 3
 RECHERCHE_PREFIXE_SEUIL_MAX = 3
 RECHERCHE_PREFIXE_SEUIL_PROXIMITE = 0.2
 
+def special_entity(m: str):
+    if m.isalpha:
+      return False
+    else: 
+      return True
 
+# retourne le mot dans l'index correspondant si il appartient à la liste et sinon appelle la fonction de liste de candidats
+def in_index(index_file:str, m: str):
+  with open(index_file, "r") as f:
+    for line in f:
+      line = line.strip()
+      if m == line:
+        return True
+    return False
+    
+def candidate_list(m: str):
+   return 0
 
 def compare_par_prefixe(m1:str, m2:str, 
                         seuil_min:float = RECHERCHE_PREFIXE_SEUIL_MIN,
