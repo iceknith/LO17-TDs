9379e18caa761f9d7cc2df4bf9b87a695d6a6e46
Iceknith <dimi.ma@laposte.net>
2026-03-26 16:06:13 +0100

    added compare par prefixe

---
+++ script/correcteur_orthographique/correcteur_orthographique.py
@@ -0,0 +1,19 @@
+
+
+RECHERCHE_PREFIXE_SEUIL_MIN = 3
+RECHERCHE_PREFIXE_SEUIL_MAX = 3
+RECHERCHE_PREFIXE_SEUIL_PROXIMITE = 0.2
+
+
+
+def compare_par_prefixe(m1:str, m2:str, 
+                        seuil_min:float = RECHERCHE_PREFIXE_SEUIL_MIN,
+                        seuil_max:float = RECHERCHE_PREFIXE_SEUIL_MAX) -> float:
+    l1 = len(m1); l2 = len(m2)
+    
+    if (min(l1, l2) <= RECHERCHE_PREFIXE_SEUIL_MIN): return 0
+    if (abs(l1 - l2) >= RECHERCHE_PREFIXE_SEUIL_MAX): return 0
+    
+    i = 0
+    while i < min(l1, l2) and m1[i] == m2[i]: i += 1
+    return i/max(l1,l2)
